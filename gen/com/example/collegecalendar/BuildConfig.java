165e0fb0d2326f582ca1fb0dc4054934fb9e2cad
jacksonhh <jackson94h@gmail.com>
2015-06-24 01:22:36 -0300

    insercao dos arquivos no repositorio

---
+++ gen/com/example/collegecalendar/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.collegecalendar;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/collegecalendar/Cadastro.java
@@ -0,0 +1,15 @@
+package com.example.collegecalendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Cadastro extends Activity{
+	 protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.cadastro);
+	    }
+}
+
+
+    
